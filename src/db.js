76b7a97d465e9b59da13aa86db787ef9f0709692
Alyson Consuli <consulialyson@gmail.com>
2022-06-07 16:41:41 -0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import pg from 'pg'
+
+const { Pool } = pg
+
+const user = 'postgres';
+const password = process.env.DATABASE_PASSWORD;
+const host = 'localhost';
+const port = 5432;
+const database = process.env.DATABASE_URL;
+
+const db = new Pool({
+    user,
+    password,
+    host,
+    port,
+    database
+})
+
+export default db
