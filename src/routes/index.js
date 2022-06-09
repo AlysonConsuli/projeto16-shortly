ede209f46fa16357056e00b7fd24c4aa049c069f
Alyson Consuli <consulialyson@gmail.com>
2022-06-09 16:25:03 -0300

    feat: get users by id w/ users middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import authRouter from "./authRouter.js";
 import urlRouter from "./urlRouter.js";
+import usersRouter from "./usersRouter.js";
 
 const router = Router()
 router.use(authRouter)
 router.use(urlRouter)
+router.use(usersRouter)
 
 export default router
