147ef07d9e0c07666b4fed6256940db618abe29d
Alyson Consuli <consulialyson@gmail.com>
2022-06-09 16:41:09 -0300

    feat: get ranking

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/usersController.js";
+import { getRanking, getUsers } from "../controllers/usersController.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 import { usersMiddleware } from "../middlewares/usersMiddleware.js";
 
 const usersRouter = Router()
 
 usersRouter.get('/users/:id', tokenValidation, usersMiddleware, getUsers)
+usersRouter.get('/ranking', getRanking)
 export default usersRouter
