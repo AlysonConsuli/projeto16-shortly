f53921b4af0b60e7eae6d2dd8cc6c178176eb88a
Mateus Brasil <Mateus-code-98>
2022-06-21 11:50:42 -0300

    Preparando revisão de código

--- src/schemas/urlSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-export const urlSchema = joi.object({
-    url: joi.string().required().uri()
-})
-
-export const urlIdSchema = joi.object({
-    id: joi.number().integer().required()
-})
